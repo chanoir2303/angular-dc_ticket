f1317ced6dd097841b0906741afc614daa98295c
ksel8 <kevselen@gmail.com>
2021-06-24 16:44:23 +0200

    adjustement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BacklogViewComponent} from "./backlog-view/backlog-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'backlog', component: BacklogViewComponent },
+  // { path: '', redirectTo: '/backlog', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
